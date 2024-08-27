6b5309c58a615fc687308897c89d85967b1de953
DongHyeon Yoon <128972134+donghyeon0619@users.noreply.github.com>
2024-08-27 14:42:29 +0900

    http 요청 헤더,파라미터 컨트롤러 구현

---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,29 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@Slf4j
+@RestController
+public class RequestParamController {
+
+    /**
+     * IOException 발생할 수 있으므로 넘겨줘야함
+     * 반환 타입이 없으면서 이렇게 응답에 값을 넣으면 view 조회 x
+     * */
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username={}, age={}", username, age);
+
+        response.getWriter().write("ok");
+
+    }
+}
